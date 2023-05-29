c70f9ff8b94ea715386307c15dc970670cda66e7
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-05-29 13:13:22 +0300

    +

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+
+
+
+const Layout = () => {
+    
+    return (
+        <div className='container'> 
+            <div>Home</div>
+        <div>Movies</div>
+        </div>
+    )
+}
+
+
+export default Layout
