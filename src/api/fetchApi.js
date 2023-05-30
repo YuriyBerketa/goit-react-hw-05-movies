08509b5c21f6758e751347d8ce80303eed19bb08
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-05-30 09:59:25 +0300

    -

---
+++ src/api/fetchApi.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const API_KEY = '414e626a6dd93e46a20c42c05170abc8';
+axios.defaults.baseURL = `https://api.themoviedb.org/3`;
+
+export const fetchTrendingApi = async () => {
+  const { data } = await axios.get(
+    `/trending/movie/day?api_key=${API_KEY}&page=1`
+  );
+  return data;
+};
+export const fetchSearchApi = async query => {
+  const { data } = await axios.get(
+    `/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`
+  );
+  return data;
+};
+export const fetchMovieDetails = async id => {
+  const { data } = await axios.get(
+    `/movie/${id}?api_key=${API_KEY}&language=en-US`
+  );
+  return data;
+};
+export const fetchMovieGenres = async () => {
+  const { data } = await axios.get(
+    `/genre/movie/list?api_key=${API_KEY}&language=en-US`
+  );
+  return data;
+};
+export const fetchCast = async id => {
+  const { data } = await axios.get(
+    `/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
+  );
+  return data;
+};
+export const fetchReview = async id => {
+  const { data } = await axios.get(
+    `/movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return data;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,39 @@
+import { fetchTrendingApi } from "api/fetchApi";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom"
 
 
 const Home = () => {
+    const [trendings, setTrendings] = useState([]);
+    console.log(trendings);
+    useEffect(() => {
+        async function moviesTrendFetch() {
+            try {
+                const data = await fetchTrendingApi();
+                setTrendings([data.results]);
+                
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        moviesTrendFetch();
+    },[])
 
-
-    return(<div> компонент Home, домашня сторінка зі списком популярних кінофільмів.</div >)
+    return (
+        
+        trendings.map(movie => {
+            return (
+            
+            <Link key={movie.id} to={`${movie.id}`}>
+                {/* <div key={movie.id} to={`/movies/${movie.id}`}></div> */}
+                    <img src={movie.poster_path} alt={movie.title} />
+                </Link>
+            
+        )}
+        )
+    )
+        // <div> компонент Home, домашня сторінка зі списком популярних кінофільмів.</div >)
 }
 
 export default Home;
